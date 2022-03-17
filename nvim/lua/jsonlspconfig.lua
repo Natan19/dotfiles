811be3750a1edbff1ce30151d0c1fafabc5faad5
Natan19 <nathanpereira.21@hotmail.com>
2022-03-17 16:46:31 -0300

    feat: save lsp name

--- nvim/lua/jsonlspconfig.lua
+++ nvim/lua/jsonlspconfig.lua
@@ -1,4 +1,5 @@
 --Enable (broadcasting) snippet capability for completion
+--The lsp is vscode-langservers-extracted
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
