81ac080b3952680779336bbd0c018ae978f1dc9e
Natan19 <nathanpereira.21@hotmail.com>
2023-10-08 23:15:16 -0300

    feat: update neovim configs

--- .config/nvim/lua/jsonlspconfig.lua
+++
@@ -1,6 +0,0 @@
---Enable (broadcasting) snippet capability for completion
---The lsp is vscode-langservers-extracted
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-require('lspconfig').jsonls.setup{ }
